1158d2d49fa9ea7e6de93544006d80282c7f9e73
Shahzain <qureshishahzain@gmail.com>
2026-05-10 10:46:58 +0200

    chore: apply initial Nixor client branding

--- apps/web/src/branding.ts
+++ apps/web/src/branding.ts
@@ -7,14 +7,14 @@ Please see LICENSE files in the repository root for full details.
 
 import SdkConfig from "./SdkConfig.ts";
 
-const ELEMENT_BRAND = "Element";
+const NIXOR_BRAND = "Nixor Connect";
 
 /**
  * Returns whether the app is currently branded.
- * This is currently a naive check of whether the `brand` config starts with the substring `Element ` or is the literal `Element`,
- * which correctly covers `Element` (release), `Element Nightly` & `Element Pro`.
+ * This is currently a naive check of whether the `brand` config starts with the substring `Nixor Connect ` or is the literal `Nixor Connect`,
+ * which correctly covers `Nixor Connect` (release), `Nixor Connect Nightly` & `Nixor Connect Pro`.
  */
-export const isElementBranded = (): boolean => {
+export const isNixorBranded = (): boolean => {
     const brand = SdkConfig.get("brand");
-    return brand === ELEMENT_BRAND || brand.startsWith(ELEMENT_BRAND + " ");
+    return brand === NIXOR_BRAND || brand.startsWith(NIXOR_BRAND + " ");
 };
